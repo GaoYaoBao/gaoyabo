d2b7f535a4099b5769e011e3a4c5508a74eaa014
ZhangYouYu0 <1031398396@qq.com>
2020-12-15 19:32:46 +0800

    第二次提交

--- app/src/main/java/first/test/qimo/MainActivity.java
+++ app/src/main/java/first/test/qimo/MainActivity.java
@@ -11,33 +11,5 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTvText;
-    int sum=3;
-    private Handler handler=new Handler(){
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            if(msg.what==1){
-                mTvText.setText("倒计时"+sum);
-                sum--;
-                if(sum==0){
-                    startActivity(new Intent(MainActivity.this,LoadActivity.class));
-                }else{
-                    handler.sendEmptyMessageDelayed(1,1000);
-                }
-            }
-        }
-    };
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initView();
-    }
-
-    private void initView() {
-        mTvText = (TextView) findViewById(R.id.tv_text);
-        handler.sendEmptyMessageDelayed(1,1000);
-
-    }
+  //哈哈写的什么玩意?
 }
