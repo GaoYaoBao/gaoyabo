a7b51d08795dcc8a2dd120dbfb80bd469bc093e9
gyb060411 <76033386+gyb060411@users.noreply.github.com>
2020-12-15 20:21:41 +0800

    Update MainActivity.java

--- app/src/main/java/first/test/qimo/MainActivity.java
+++ app/src/main/java/first/test/qimo/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
   //哈哈写的什么玩意?
 <<<<<<< HEAD
 	//你好牛
+	//电饭锅昏迷是和GV用户将好几年
 =======
  //你是个什么东西
 >>>>>>> 1c908ff426e334b528cdd2024305158339571ee0
